3f282fd190f4de4b1f99fe3e6c298d88d90e1575
猫猫虫 <8167802+crying-cat@user.noreply.gitee.com>
2021-06-19 00:51:53 +0800

    aa

--- py_admin/main.py
+++ py_admin/main.py
@@ -1,28 +1,38 @@
 import welcome
+import search
 import test1
 import test2
 import test3
 import test4
-import test5
 import test6
 import test7_9
-welcome.welcome()
+# welcome.welcome()
+choose='-1'   # 实现子功能的循环执行
 while 1:
-    print('''
-    1.查询操作(单表查询、复杂查询)
-    2.单表查询
-    3.插入学生数据/选课信息
-    4.更改学生专业信息/成绩信息
-    5.查询特定学生的选课信息
-    6.查询特定学院/专业的所有学生
-    7.查询小于60分的所有学生
-    8.查询选修了某门课的所有学生的信息及成绩
-    9.查询某姓的所有学生
-    '''
-    )
-    choose=input("输入要选择的功能")
+    if choose=='-1':    # 子功能不循环执行
+        print('''
+                       1.查询操作(单表查询、复杂查询)
+                       3.插入学生数据/选课信息
+                       4.更改学生专业信息/成绩信息
+                       '''
+              )
+        choose=input("输入要选择的功能")
+
     if choose == '1':
-        test1.test1()
+        print("                  查询操作子界面")
+        print("           ========================")
+        print('''
+            1.全表查询
+            2.查询特定学生的选课信息
+            3.查询特定学院/专业的所有学生
+            4.查询小于60分的所有学生
+            5.查询选修了某门课的所有学生的信息及成绩
+            6.查询某姓的所有学生
+            ''')
+        a=eval(input())
+        b=search.search(a)
+        if b!=0:
+            continue
     elif choose == '2':
         test2.test1()
     elif choose == '3':
@@ -30,7 +40,8 @@ while 1:
     elif choose=='4':
         test4.test1()
     elif choose == '5':
-        test5.test1()
+        # test5.test1()
+        print("aaa")
     elif choose == '6':
         test6.test1()
     elif choose == '7':
@@ -39,6 +50,7 @@ while 1:
         test7_9.test2()
     elif choose == '9':
         test7_9.test3()
+    choose=-1   #实现
     a = input("输入0退出，其他键继续")
     if a == '0':
         break
--- py_admin/search.py
+++ py_admin/search.py
@@ -1,182 +1,141 @@
 import db
 db = db.db()
 cursor = db.cursor()
-#
 def quanbiao():
     try:
-        sql_1 ="select  * from stu where sno="
+        sql_1 = "select  * from stu where sno="
         sql_2 = "select * from sc"
         sql_3 = "select * from major where mno="
         sql_4 = "select * from academy where yno= "
         sql_5 = "select * from course where cno = "
-        a=eval(input("请输入要查询哪一个表 1(学生表),2(成绩表),3(专业表),4(学院表),5(选修课程表)"))
-        if a==1:
+        a = eval(input("请输入要查询哪一个表 1(学生表),2(成绩表),3(专业表),4(学院表),5(选修课程表)"))
+        if a == 1:
             detail = input("输入学生学号")
             sql_1 = sql_1 + detail
             print(sql_1)
             print("  学号  姓名      1性别     出生日期    籍贯    民族    年级    专业号")
             cursor.execute(sql_1)
-        elif a==2:
-            sno=input("输入学号")
-            cno=input("输入专业号")
-            sql_2=sql_2+' where sno='+sno + ' and cno = '+cno
+        elif a == 2:
+            sno = input("输入学号")
+            cno = input("输入专业号")
+            sql_2 = sql_2 + ' where sno=' + sno + ' and cno = ' + cno
             print(" 学号  专业号  分数")
             # print(sql_2)
             cursor.execute(sql_2)
-        elif a== 3:
-            mno=input("输入专业号")
-            sql_3=sql_3+ mno
+        elif a == 3:
+            mno = input("输入专业号")
+            sql_3 = sql_3 + mno
             print(sql_3)
             print(" 专业号    专业名   专业类别   所在学院")
             cursor.execute(sql_3)
         elif a == 4:
-            yno=input("输入学院号")
+            yno = input("输入学院号")
             print(" 学院号  学院名  院长")
-            sql_4=sql_4+yno
+            sql_4 = sql_4 + yno
             print(sql_4)
             cursor.execute(sql_4)
         elif a == 5:
-            cno=input("请输入课程号")
-            sql_5=sql_5+cno
+            cno = input("请输入课程号")
+            sql_5 = sql_5 + cno
             print("课程号    专业名 学分 所属学院")
             cursor.execute(sql_5)
-        else: print("请输入数字1-5")
-        while 1:
-            res = cursor.fetchone()
-            if res is None:
-                break
-            print(res)
-        # result1 = cursor.fetchall()
-        print(res)
-
+        else:
+            print("请输入数字1-5")
     except Exception as e:
         print(e)
-    db.close()
-# 第五个功能
+
+# 查询特定学生的选课信息
 def search1():
     try:
-        a=input("输入学生的名字")
+        a = input("输入学生的名字")
         sql = '''
             select cname 选修课名 ,grade 成绩 from course,sc
             where course.cno=sc.cno and sc.sno=(
-	        select sno from stu 
-	        where sname="%s")
-        '''%(a)
+            select sno from stu 
+            where sname="%s")
+        ''' % (a)
         # print(sql)
         cursor.execute(sql)
         # print(query)
     except IOError:
         print("err")
-# 第六个功能
+# 查询特定学院/专业的所有学生
 def search2():
     try:
-        # 数据库导入配置
-        import db
-        db = db.db()
-        cursor = db.cursor()
-        count=0
-        a=eval(input("请输入1(查询特定学院)、2查询特定专业"))
-        if a==1:
-            str=input("输入学院名")
-            sql='''
+        a = eval(input("请输入1(查询特定学院)、2查询特定专业"))
+        if a == 1:
+            str = input("输入学院名")
+            sql = '''
             select sname 学生姓名
             from stu,major,academy
             where stu.mno=major.mno AND 
             major.yno=academy.yno and
             major.yno =(SELECT yno from
-		        academy where yname='%s'
-            )'''%(str)
+                academy where yname='%s'
+            )''' % (str)
 
-        if a==2:
-            str=input("请输入专业名")
-            sql='''
+        if a == 2:
+            str = input("请输入专业名")
+            sql = '''
             select sname 学生名,mname 专业名
             from stu,major
-            where stu.mno=major.mno and major.mname='%s' '''%(str)
+            where stu.mno=major.mno and major.mname='%s' ''' % (str)
         # print(sql)
         cursor.execute(sql)
-        while 1:
-            # print("成功")
-            count=count+1
-            res = cursor.fetchone()
-            if res is None:
-                break
-            print(res)
-        # result1 = cursor.fetchall()
-        print(res)
-        # print("总人数",str(count))
-        db.close()
     except IOError:
         print("err")
-
-# 第七个功能
-def test1():
+# 查询小于60分的所有学生
+def search3():
     try:
-        # 数据库导入配置
-        import db
-        db = db.db()
-        cursor = db.cursor()
-        query='''select stu.sno,sname,sc.grade
+        query = '''select stu.sno,sname,sc.grade
                 from stu,sc
                 where stu.sno=sc.sno and sc.grade<60'''
         cursor.execute(query)
-        # print(query)
-        while 1:
-            res = cursor.fetchone()
-            if res is None:
-                break
-            print(res)
-        # result1 = cursor.fetchall()
-        print(res)
     except IOError:
         print("err")
-
-# 第八个功能
-def test2():
+# 查询选修了某门课的所有学生的信息及成绩
+def search4():
     try:
-        # 数据库导入配置
-        import db
-        db = db.db()
-        cursor = db.cursor()
-        str=input("输入课程名")
+        str = input("输入课程名")
         query = '''select sname 学生姓名,stu.sno 学号 ,course.cno 课程号,grade 成绩 from 
                     stu,sc,course
                     where sc.sno = stu.sno 
                     and sc.cno=(select cno FROM
-	                course where cname='%s')
+                    course where cname='%s')
                     and sc.cno=course.cno
-                    '''%(str)
+                    ''' % (str)
         cursor.execute(query)
-        # print(query)
-        while 1:
-            res = cursor.fetchone()
-            if res is None:
-                break
-            print(res)
-        # result1 = cursor.fetchall()
-        print(res)
     except IOError:
         print("err")
-
-# 第9个功能
-def test3():
+# 查询某姓的所有学生
+def search5():
     try:
-        # 数据库导入配置
-        import db
-        db = db.db()
-        cursor = db.cursor()
         index = input("请输入学生的姓")
         query = 'select sname from stu where sname like "{}%"'.format(index)
         # print(query)
         cursor.execute(query)
-        # print(query)
-        while 1:
-            res = cursor.fetchone()
-            if res is None:
-                break
-            print(res)
-        # result1 = cursor.fetchall()
-        print(res)
-        db.close()
     except IOError:
         print("err")
+def search(a):
+    if a == 1:
+        quanbiao()
+    if a == 2:
+        search1()
+    elif a==3:
+        search2()
+    elif a == 4:
+        search3()
+    elif a == 5:
+        search4()
+    elif a == 6:
+        search5()
+    while 1:
+        print("执行成功")
+        res = cursor.fetchone()
+        if res is None:
+            break
+        print(res)
+    # result1 = cursor.fetchall()
+    print(res)
+    isquit=eval(input("要继续执行查询操作吗 输0退出"))
+    return isquit
